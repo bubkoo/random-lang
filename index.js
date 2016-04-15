b432f7e7e9755761a7d174743c69992b797561c7
bubkoo <bubkoo.wy@gmail.com>
2016-04-16 01:54:46 +0800

    v1.0.0

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 'use strict';
 
 var languages = require('languages-list');
-var pickSome  = require('pick-some');
+var pickItem  = require('pick-item');
 
 module.exports = function () {
-
-  var ret = pickSome(languages, { count: 1 });
-
-  return ret && ret[0];
+  return pickItem(languages, { count: 1 });
 };
